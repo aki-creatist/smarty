d2979fc2329fc15ac27e4b5c3e8eba3bf6faf304
AKI <aki.doubledutch@gmail.com>
2018-04-28 23:30:57 +0900

    Update AuthController.php

--- project/php_libs/controller/AuthController.php
+++ project/php_libs/controller/AuthController.php
@@ -37,15 +37,19 @@ class AuthController extends Database
 
     public function getHashedPW($password)
     {
-        $cost = 10;
+        //コストパラメーターとして2桁の数字を設定する
+        $cost = 10; //数字を大きくすると堅牢になるが、システムの負荷が増大する
+        
         // PHP7
         //$rondom = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
         // PHP5
-        $rondom = random_bytes(16);
-        $base64_encode = base64_encode($rondom);
-        $salt = strtr($base64_encode, '+', '.');
-        $salt = sprintf("$2y$%02d$", $cost) . $salt;
-        $hash = crypt($password, $salt);
+        $rondom = random_bytes(16);                  //ソルトを作成するため関数でランダムなデータを生成
+        $base64_encode = base64_encode($rondom);     //さらに`base64_encode`で文字列を半角の英数字列にする (`+`が入る)
+        $salt = strtr($base64_encode, '+', '.');     //そのため`strstr関数`で `.` に変換する
+        $salt = sprintf("$2y$%02d$", $cost) . $salt; //`sprintf関数`で`%02d`の部分に`10`が入る (`$2y$10$文字列`となる)
+        
+        //ハッシュ値を生成する                           //crypt関数は一つ目の引数にパスワードを、二つ目の引数にソルト指定する
+        $hash = crypt($password, $salt);             //ソルトの先頭の文字列によりどのハッシュアルゴリズムを利用するか決められる (今回は`$2y$`を利用)
         return $hash;
     }
 
